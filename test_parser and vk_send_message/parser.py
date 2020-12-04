ba24a8c5aa950b93ccbd8f099bdb5978bc5089c8
ALepilin <70595944+LpilinAlexandr@users.noreply.github.com>
2020-12-04 13:55:07 +0500

    Update parser.py
    simple proxy parser

--- test_parser and vk_send_message/parser.py
+++ test_parser and vk_send_message/parser.py
@@ -1,60 +1,52 @@
-from pprint import pprint
-
-import bs4
 import requests
-import time
-import re
-import http.client
+from lxml import html
+from io import StringIO
 
-http.client._MAXHEADERS = 1000
+class Proxy:
+
+    """
+    Получает прокси для парсера
+    """
+
+    proxy_url = 'https://www.ip-adress.com/proxy-list'
+
+    def __init__(self):
+        self.new_list = []
+        response = requests.get(self.proxy_url)
+        content = html.parse(StringIO(response.text)).getroot()
+        table = content.find_class('htable proxylist')
+        link_iter = html.iterlinks(table[0])
+        text_value = table[0].xpath('//tr/td/text()')
+        self.proxy_list = [link[0].text_content() for link in link_iter]
+        for i in range(0, len(text_value), 3):
+            port = str(text_value[i])
+            proxy = self.proxy_list[0]
+            self.new_list.append(f"{proxy}{port}")
+            self.proxy_list.pop(0)
 
 
-MY_REGULAR = re.compile(r"href=\"([\w\S]+)\"")
 
-URL = 'https://focus-auto.ru'
-SHOP = '/shop/ford/'
+    def get_proxy(self, url):
+        for proxy in self.new_list:
+            print('ПРобую: ', proxy)
+            proxies =  {
+                "http": f'http://{proxy}',
+                "https": f'http://{proxy}'
+            }
+            try:
+                response = requests.get(url, proxies=proxies, timeout=3)
+                if response.status_code == 200:
+                    return proxy
+            except:
+                # print('ПРокси не валидный', proxies)
+                continue
+        print('Не осталось проксей :(')
 
-MY_DICT = {}
-
-def get_request(url, page=''):
-    result = requests.get(f'https://focus-auto.ru{url}{page}')
-    return result.text
-
-
-def next_page(url):
-    """На каждой категории берёт всё со страницы и так по каждой странице"""
-    i = 0
-    next = True
-    product_list = []
-    while next:
-        i += 1
-        new_page = f"?page={i}"
-        page = get_request(url=url, page=new_page)
-        html_doc = bs4.BeautifulSoup(page, features='html.parser')
-        final_all_tags = html_doc.find_all('h3', {'class': 'product_name'})
-        if len(final_all_tags) < 1:
-            next = False
-        for _ in final_all_tags:
-            matches = re.search(MY_REGULAR, str(_))
-            product_list.append(matches[1])
-    return product_list
-
-html_doc = bs4.BeautifulSoup(get_request(SHOP), features='html.parser')
-all_tags = html_doc.find_all('a', {'class': 'catalog_item'})
-
-for tag in all_tags:
-    time.sleep(0.3)
-    new_page = tag.get('href')
-    car_name = new_page.split('/')[-2]
-    MY_DICT[car_name] = []
-    new_page_items = get_request(new_page)
-    html_doc = bs4.BeautifulSoup(new_page_items, features='html.parser')
-    new_all_tags = html_doc.find_all('a', {'class': 'catalog_item'})
-    for new_tag in new_all_tags:
-        time.sleep(0.3)
-        new_tag.get('href')
-        product_list = next_page(url=new_tag.get('href'))
-        MY_DICT[car_name].extend(product_list)
-    print(car_name, len(MY_DICT[car_name]))
-    # pprint(MY_DICT[car_name])
-
+    def check_proxy_work(self, url):
+        """
+        Зайти с валидного прокси на яндекс интернетометр и найти там свой айпи
+        """
+        proxy = self.get_proxy(url)
+        print(proxy)
+        response = requests.get('https://2ip.ru/', proxies=proxy)
+        print(response.content)
